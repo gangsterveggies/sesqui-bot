45afbb7899f05e5d9326ed086bbe5693fda80cbc
Miguel Ferreira <miguelferreira108@gmail.com>
2015-04-05 17:36:57 +0100

    Initial work on Alpha-Beta

---
+++ ab.cpp
@@ -0,0 +1,16 @@
+#include "ab.h"
+
+unordered_map<board, list<board> > sons;
+
+Move alphabeta(board b) {
+}
+
+int MaxValue(board b, int alpha, int beta) {
+}
+
+int MinValue(board b, int alpha, int beta) {
+}
+
+list<board>& Successors(board b) {
+  //use an auxiliar key/value store?
+}
